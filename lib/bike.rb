b184932d667c378efd9f138a6c7585279d216b29
Proskurina <yproskurina@yahoo.com>
2015-09-03 12:28:31 +0100

    Add test for broken and methods report_broken and broken?

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,5 +1,22 @@
 class Bike
-  def working?
-    true
+  
+  attr_accessor :status
+
+  def initialize(status = true)
+  	@status = status
   end
+
+  def working?
+    @status
+  end
+
+  def report_broken
+  	@status = false
+  end
+
+  def broken?
+  	not working?
+  end
+
 end
+ 
