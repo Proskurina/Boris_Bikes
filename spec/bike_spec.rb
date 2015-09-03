863700e71cd3b27f0cbba4a43974b0a0a009fdf6
Proskurina <yproskurina@yahoo.com>
2015-09-03 21:22:01 +0100

    Added tests for Bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,11 +1,22 @@
 require 'bike'
 
 describe Bike do
+  
+  it "#new works" do
+    expect(Bike).to respond_to :new
+  end
+
   it { is_expected.to respond_to :working?}
+  it { is_expected.to respond_to :broken?}
 
   it 'can be reported broken' do
   	subject.report_broken
   	expect(subject).to be_broken
+  end
+
+  it "if reported broken then it is not workig" do
+  	subject.report_broken
+  	expect(subject).not_to be_working
   end
 
 end 
